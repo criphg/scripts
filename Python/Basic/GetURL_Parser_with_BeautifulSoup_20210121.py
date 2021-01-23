584917bde146d8c495415f2c60eb2dfd0739750d
criph <criphg@gmail.com>
2021-01-22 23:45:51 -0300

    New Python additions

--- Python/Basic/GetURL_Parser_with_BeautifulSoup_20210121.py
+++ Python/Basic/GetURL_Parser_with_BeautifulSoup_20210121.py
@@ -9,19 +9,29 @@ Created on Thu Jan 21 23:23:11 2021
 import urllib
 from bs4 import BeautifulSoup
 
-fhand = urllib.request.urlopen("http://www.ign.com")
+def get_url(url_v):
+    fhand = urllib.request.urlopen(url_v)
 
-html = fhand.read()
-soup = BeautifulSoup(html, 'html.parser')
+    html = fhand.read()
+    soup = BeautifulSoup(html, 'html.parser')
 
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href',None))
+    tags = soup('a')    
+    for tag in tags:
+        print(tag.get('href',None))
+        
+        counts = dict()
+        for line in fhand:
+            words = line.decode().split()
+            for word in words:
+                counts[word] = counts.get(word, 0) + 1
+        #print(counts)
 
-counts = dict()
-for line in fhand:
-    words = line.decode().split()
-    for word in words:
-        counts[word] = counts.get(word, 0) + 1
-#print(counts)
 
+
+try:
+    url_in = input("Type URL: >")
+    get_url(url_in)
+except :
+    print("URL not found")
+#url_in = "http://www.ign.com"
+#get_url(url_in)
